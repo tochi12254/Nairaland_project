b90d2fedf89b51ebed17a189c3ef01a4200da303
anneyomeje <anne.omeje@gmail.com>
2024-03-26 12:51:33 +0100

    made some changes

--- login.js
+++ login.js
@@ -2,7 +2,7 @@ const loginButton = document.querySelector('#login');
 
 function login(e) {
     e.preventDefault();
-    window.location.href = 'authenticated-page.html'
+    window.location.href = 'authenticated-page.html';
 }
 
 loginButton.addEventListener('click', login);
